2b4db2e2e17c0330f4db7f343bf0688afa71432d
Tquilla. <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-02-25 21:42:23 +0700

    Refactor xytz package option for flake support

--- nix/nixos-module.nix
+++ nix/nixos-module.nix
@@ -1,4 +1,4 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, self... }:
 
 let
   cfg = config.programs.xytz;
@@ -7,8 +7,8 @@ in {
     enable = lib.mkEnableOption "xytz tool";
     package = lib.mkOption {
       type = lib.types.package;
-      default = pkgs.callPackage ../. {}; # Akan di-override oleh flake
-      description = "Package xytz yang akan diinstal.";
+      default = self.packages.${pkgs.system}.default;
+      description = "Which Package xytz installed.";
     };
   };
 
