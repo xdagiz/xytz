1f09b522c985f03828e7fec3f3de2bfe2f3289ba
Dagmawi Ali <xdagizjr2112@gmail.com>
2026-03-03 01:23:59 +0300

    feat: nix support (#16)

---
+++ flake.nix
@@ -0,0 +1,56 @@
+{
+  description = "xytz - YouTube from your terminal";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+    in
+    {
+      packages = forAllSystems (system:
+        let pkgs = nixpkgsFor.${system}; in {
+          default = pkgs.buildGoModule {
+            pname = "xytz";
+            version = "unstable";
+            src = pkgs.lib.cleanSource ./.;
+            vendorHash = "sha256-loLssmeKd6paM2cMXzGE+xRozgOzlpOLARfP9+ZruGI=";
+            doCheck = false;
+            nativeBuildInputs = [ pkgs.makeWrapper ];
+            postInstall = ''
+              wrapProgram "$out/bin/xytz" \
+                --prefix PATH : ${pkgs.lib.makeBinPath [
+                  pkgs.yt-dlp
+                  pkgs.ffmpeg
+                  pkgs.mpv
+                ]}
+            '';
+
+            meta = with pkgs.lib; {
+              description = "A TUI app for searching and downloading YouTube videos";
+              homepage = "https://github.com/xdagiz/xytz";
+              license = licenses.mit;
+              mainProgram = "xytz";
+            };
+          };
+        });
+
+      apps = forAllSystems (system: {
+        default = flake-utils.lib.mkApp {
+          drv = self.packages.${system}.default;
+        };
+      });
+
+      devShells = forAllSystems (system:
+        let pkgs = nixpkgsFor.${system}; in {
+          default = pkgs.mkShell {
+            packages = [ pkgs.go_1_25 ];
+          };
+        });
+    };
+}
